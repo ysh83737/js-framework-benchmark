12e0847b91e8628096d4d587a6264515fa9db725
ShawnYuan <ysh83737@hotmail.com>
2024-04-29 17:44:56 +0800

    feat: 添加show-table测试

---
+++ frameworks/keyed/show-table/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ElButton, ElTable, ElTableColumn } from 'element-plus'
+import 'element-plus/es/components/button/style/css'
+import 'element-plus/es/components/table/style/css'
+import 'element-plus/es/components/table-column/style/css'
+
+import { ref } from 'vue'
+import { datas, MOCK_COUNT } from './data'
+
+/**
+ * 多个<ElTable />实例，但通过v-if控制每次仅存在1个
+ */
+defineOptions({
+  name: 'IfTable'
+})
+
+const index = ref(0)
+
+function onChangeData() {
+  index.value = 1
+}
+</script>
+<template>
+  <div>
+    <ElButton id="run" @click="onChangeData">换一组数据</ElButton>
+    <template v-for="(item) in MOCK_COUNT" :key="item">
+      <ElTable v-show="item - 1 === index" :data="datas[item - 1]">
+        <ElTableColumn prop="id" label="ID" />
+        <ElTableColumn prop="label" label="Label" />
+      </ElTable>
+    </template>
+  </div>
+</template>
---
+++ frameworks/keyed/show-table/src/data.ts
@@ -0,0 +1,97 @@
+let ID = 1
+
+function _random(max: number) {
+  return Math.round(Math.random() * 1000) % max
+}
+
+export function buildData(count = 1000) {
+  const adjectives = [
+    'pretty',
+    'large',
+    'big',
+    'small',
+    'tall',
+    'short',
+    'long',
+    'handsome',
+    'plain',
+    'quaint',
+    'clean',
+    'elegant',
+    'easy',
+    'angry',
+    'crazy',
+    'helpful',
+    'mushy',
+    'odd',
+    'unsightly',
+    'adorable',
+    'important',
+    'inexpensive',
+    'cheap',
+    'expensive',
+    'fancy'
+  ]
+  const colours = [
+    'red',
+    'yellow',
+    'blue',
+    'green',
+    'pink',
+    'brown',
+    'purple',
+    'brown',
+    'white',
+    'black',
+    'orange'
+  ]
+  const nouns = [
+    'table',
+    'chair',
+    'house',
+    'bbq',
+    'desk',
+    'car',
+    'pony',
+    'cookie',
+    'sandwich',
+    'burger',
+    'pizza',
+    'mouse',
+    'keyboard'
+  ]
+  const data: Item[] = []
+  for (let i = 0; i < count; i++)
+    data.push({
+      id: ID++,
+      label:
+        adjectives[_random(adjectives.length)] +
+        ' ' +
+        colours[_random(colours.length)] +
+        ' ' +
+        nouns[_random(nouns.length)]
+    })
+  return data
+}
+
+export const MOCK_COUNT = 2
+export const MOCK_PAGE_SIZE = 1000
+
+export interface Item {
+  id: number;
+  label: string
+}
+export const datas = Array(MOCK_COUNT).fill(1).map(() => buildData(MOCK_PAGE_SIZE))
+
+export function changeIndex(cb: () => number) {
+  return new Promise((r) => {
+    setTimeout(() => {
+      const index = cb()
+      console.log('推送第', index + 1, '组数据')
+      if (index + 1 < MOCK_COUNT) {
+        r(changeIndex(cb))
+      }
+      r(null)
+    }, 1000);
+  })
+}
